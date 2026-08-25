10016e3494bf15ba0ab58e6acdb81be8598c8bae
halvvin <halvvin@users.noreply.github.com>
2026-08-25 04:51:59 +0300

    Add missing BoxScope import

--- src/android/app/src/main/java/com/openminis/app/ui/browser/SmartBrowserScreen.kt
+++ src/android/app/src/main/java/com/openminis/app/ui/browser/SmartBrowserScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
